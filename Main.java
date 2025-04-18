f098fbd198acde3de4ee58a44cd08773a568afa9
ChristopherChantres <chris.chantres@gmail.com>
2025-04-18 00:32:55 -0600

    bugfix: scanner was declared globally to prevent an ERROR while executing continurarEvento().\nData for Registration & Login are available to be stored in the DB (and for any further logic).

--- Main.java
+++ Main.java
@@ -1,30 +1,59 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final Scanner scanner = new Scanner(System.in); // Scanner global
 
+    public static void main(String[] args) {
+        limpiarConsola();
+
+        // Obtener datos del usuario para el registro
+        System.out.println("------------------ REGISTRO DE USUARIO ------------------\n");
+        String[] datos_del_usuario_registro = getDatosDelUsuario();
+        String username_registro = datos_del_usuario_registro[0];
+        String password_registro = datos_del_usuario_registro[1];
+        
+        /*
+         * Aquí puedes agregar el código para insertar el username_registro y password_registro en la base de datos.
+         */
+        mensaje("Usuario registrado correctamente: " + username_registro, TipoDeMensaje.EXITO);
+        continuarEvento();
+        
+        // Obtener datos del usuario para el login
+        System.out.println("------------------ INICIO DE SESION ------------------\n");
+        String[] datos_del_usuario_login = getDatosDelUsuario();
+        String username_login = datos_del_usuario_login[0];
+        String password_login = datos_del_usuario_login[1];
+
+        /*
+         * Aquí puedes agregar el código para insertar el username_login y password_login en la base de datos.
+         */
+        mensaje("Inicio de sesion satisfactorio.", TipoDeMensaje.EXITO);
+        continuarEvento();
     }
 
     // Método para registrar un nuevo usuario
-    public static String[] registrarUsuario() {
-        Scanner scanner = new Scanner(System.in);
+    public static String[] getDatosDelUsuario() {
         String[] datos_del_usuario = new String[2];
-        System.out.println("Ingrese su username:");
+
+        // Solicitar el username
+        System.out.print("Username: ");
         String nombre = scanner.nextLine();
         if (nombre.isEmpty()) {
-            System.out.println("Error: El nombre de usuario no puede estar vacio.");
-            limpiarConsola();
-            registrarUsuario(); // Reintentar registro
+            mensaje("El username no puede estar vacio.", TipoDeMensaje.ERROR);
+            continuarEvento();
+            getDatosDelUsuario(); // Reintentar registro
         }
 
-        System.out.println("Ingrese su password:");
+        // Solicitar el password
+        // System.out.print("\n");
+        System.out.print("Password: ");
         String password = scanner.nextLine();
         if (password.isEmpty()) {
-            System.out.println("Error: El nombre de usuario y la contraseña no pueden estar vacíos.");
+            mensaje("El password no puede estar vacio.", TipoDeMensaje.ERROR);
             limpiarConsola();
-            registrarUsuario(); // Reintentar registro
+            continuarEvento();
+            getDatosDelUsuario(); // Reintentar registro
         }
-        scanner.close();
 
         // Guardar los datos del usuario en el array
         datos_del_usuario[0] = nombre;
@@ -32,6 +61,7 @@ public class Main {
         return datos_del_usuario;
     }
 
+    // -------------------- FUNCIONES DE UTILERIA -------------------- //
     public static void limpiarConsola() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
@@ -66,13 +96,24 @@ public class Main {
         }
     }
 
+    public static void continuarEvento() {
+        System.out.println("Presione Enter para continuar...");
+        scanner.nextLine();
+        limpiarConsola();
+    }
+
     public static void mostrarMenu() {
-        System.out.println("========= Bienvenido a OnlineDeal ========");
-        System.out.println("================ MENU =========="); 
-        System.out.println("1. Agregar producto");
-        System.out.println("2. Mostrar productos");
-        System.out.println("3. Guardar en archivo");
-        System.out.println("4. Pagar");
-        System.out.println("5. Salir");
+        System.out.println("=================================================");
+        System.out.println("              Bienvenido a OnlineDeal           ");
+        System.out.println("=================================================");
+        System.out.println("                     MENU                       ");
+        System.out.println("-------------------------------------------------");
+        System.out.println("|  1. Carrito                                   |");
+        System.out.println("|  2. Mostrar productos                         |");
+        System.out.println("|  3. Guardar en archivo                        |");
+        System.out.println("|  4. Pagar                                     |");
+        System.out.println("|  5. Salir                                     |");
+        System.out.println("-------------------------------------------------");
+        System.out.print("Seleccione una opción: ");
     }
 }
