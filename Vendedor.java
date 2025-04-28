d091e4bdd7b7f9a779e7130c0b516fbf8390ce95
SE7VEN07 <f.juavil2005@gmail.com>
2025-04-28 07:51:45 -0600

    Create Vendedor.java

---
+++ Vendedor.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+puvlic class Vndedor extends Usuario{
+  private String tienda;
+  private ArrayList <Producto> productosVendidos;
+
+  public Vendedor(String nombre, String correo, String contrasena, String tienda){
+    super(nombre, correo, contrasena);
+    this.tienda = tienda;
+    this.productosVendidos = new ArrayList<>();v
+  }
+}
